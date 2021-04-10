a90581660c4f2d4e64fdba4d15c6451ab3bc1dc7
saikrishnay56 <77909873+saikrishnay56@users.noreply.github.com>
2021-04-10 09:15:31 +0530

    update with order

--- DeepLearning3/Source Code/prg2.py
+++ DeepLearning3/Source Code/prg2.py
@@ -14,11 +14,13 @@ sentences = df['review'].values
 y = df['label'].values
 
 
+le = preprocessing.LabelEncoder()
+y = le.fit_transform(y)
+
 #tokenizing data
 tokenizer = Tokenizer(num_words=2000)
 tokenizer.fit_on_texts(sentences)
 #getting the vocabulary of data
-
 
 max_review_len = max([len(s.split()) for s in sentences])
 vocab_size = len(tokenizer.word_index)+1
@@ -26,13 +28,9 @@ vocab_size = len(tokenizer.word_index)+1
 sentences = tokenizer.texts_to_matrix(sentences)
 padded_docs = pad_sequences(sentences, maxlen=max_review_len)
 
-
-le = preprocessing.LabelEncoder()
-y = le.fit_transform(y)
-
 X_train, X_test, y_train, y_test = train_test_split(padded_docs, y, test_size=0.25, random_state=1000)
-#X_train_tokens = tokenizer.texts_to_sequences(X_train)
-#X_test_tokens = tokenizer.texts_to_sequences(X_test)
+X_train_tokens = tokenizer.texts_to_sequences(X_train)
+X_test_tokens = tokenizer.texts_to_sequences(X_test)
 
 
 model = Sequential()
