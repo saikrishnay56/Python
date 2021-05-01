923a3cae0700d686fdfe1f903f4dc4dee41476f4
saikrishnay56 <77909873+saikrishnay56@users.noreply.github.com>
2021-05-01 05:48:05 +0530

    program1

---
+++ DeepLearning6/Source Code/Prg1.py
@@ -0,0 +1,49 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.datasets import fashion_mnist
+import numpy as np
+
+# this is the size of our encoded representations
+encode_dimension = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+image = Input(shape=(784,))
+#The hidden layer is added here
+hidden_layer = Dense(128, activation='relu')(image)
+# "encoded" is the encoded representation of the input
+encoded = Dense(encode_dimension, activation='relu')(hidden_layer)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+# this model maps an input to its reconstruction
+autoencoder = Model(image, decoded)
+# this model maps an input to its encoded representation
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+# This is encoder model for predicting test images
+encoder = Model(image, encoded)
+encoded_input = Input(shape=(encode_dimension,))
+
+# Decoder model - take in last layer of autoencoder
+decoder_layer = autoencoder.layers[-1]
+
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+history = autoencoder.fit(x_train, x_train,
+                epochs=10,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
