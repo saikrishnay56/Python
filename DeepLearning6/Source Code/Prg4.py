28727c0432d8b45a5297747882a97b9d44fc7ec5
saikrishnay56 <77909873+saikrishnay56@users.noreply.github.com>
2021-05-01 05:51:30 +0530

    program4

---
+++ DeepLearning6/Source Code/Prg4.py
@@ -0,0 +1,96 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.datasets import fashion_mnist
+import numpy as np
+import matplotlib.pyplot as plt
+
+# this is the size of our encoded representations
+encode_dimension = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+image = Input(shape=(784,))
+#The hidden layer is added here
+hidden_layer = Dense(128, activation='relu')(image)
+# "encoded" is the encoded representation of the input
+encoded = Dense(encode_dimension, activation='relu')(hidden_layer)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+# this model maps an input to its reconstruction
+autoencoder = Model(image, decoded)
+# this model maps an input to its encoded representation
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+# This is encoder model for predicting test images
+encoder = Model(image, encoded)
+encoded_input = Input(shape=(encode_dimension,))
+
+# Decoder model - take in last layer of autoencoder
+decoder_layer = autoencoder.layers[-1]
+
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+# introducing noise
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
+
+history = autoencoder.fit(x_train_noisy, x_train,
+                          epochs=10,
+                          verbose=2,
+                          batch_size=256,
+                          shuffle=True,
+                          validation_data=(x_test_noisy, x_test_noisy))
+
+encoded_imgs = encoder.predict(x_test_noisy)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+# Plot our images and reconstructions
+n = 4  # How many images to display
+plot1 = plt.figure(figsize=(10, 5))
+for i in range(n):
+    # Display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test_noisy[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # Display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+#print((history.history.keys()))
+# Accuracy Plot
+
+plot2 = plt.figure(2)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='lower left')
+
+# "Loss Plot"
+plot3 = plt.figure(3)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
